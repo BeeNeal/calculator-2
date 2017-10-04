01601593e54f955a4db321ac67618f9d6af87a3f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-04 15:11:45 -0700

    Student made calculator

---
+++ calculator.py
@@ -0,0 +1,70 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
+def calculate(user_input):
+    """prefix calculator that evaluates user user_input
+
+    enter operator and evaluate following numbers"""
+
+    tokens = user_input.split(" ")
+    answer = "I don't understand."
+    # print tokens
+    for i in tokens[1:]:
+        try:
+            float(i)
+        except:
+            return answer
+
+    if tokens[0] == "+":
+        answer = add(float(tokens[1]), float(tokens[2]))
+
+    elif tokens[0] == "-":
+        answer = subtract(float(tokens[1]), float(tokens[2]))
+
+    elif tokens[0] == "*":
+        answer = multiply(float(tokens[1]), float(tokens[2]))
+
+    elif tokens[0] == "/":
+        answer = divide(float(tokens[1]), float(tokens[2]))
+
+    elif tokens[0] == "square":
+        answer = square(float(tokens[1]))
+
+    elif tokens[0] == "cube":
+        answer = cube(float(tokens[1]))
+
+    elif tokens[0] == "pow":
+        answer = power(float(tokens[1]), float(tokens[2]))
+
+    elif tokens[0] == "mod":
+        answer = mod(float(tokens[1]), float(tokens[2]))
+
+    elif tokens[0] == "add_mult":
+        answer = add_mult(float(tokens[1]), float(tokens[2]), float(tokens[3]))
+
+    elif tokens[0] == "add_cubes":
+        answer = add_cubes(float(tokens[1]), float(tokens[2]))
+
+
+
+
+    return answer
+
+def run_calc():
+    """loop through calculator"""
+
+    while True: # user_input != "q":
+        user_input = raw_input("Please enter your expression: ")
+        if user_input == "q":
+            break
+        print calculate(user_input)
+
+
+run_calc()
